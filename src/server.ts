5f88059bcb5884137d53f9105585548e00e346b9
Enishowk <kenshineuh@gmail.com>
2019-03-16 16:09:53 +0100

    add link

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,6 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(3000, () =>
+  console.log('Listening on port 3000! http://localhost:3000')
+);
